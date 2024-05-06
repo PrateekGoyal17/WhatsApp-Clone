9efbc25926262060bebf9d66aacff4dc78c44d0e
Prateek Goyal <goyalprateek482@gmail.com>
2024-05-06 16:20:04 +0530

    Added Uploading Status

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
 import 'package:whatsapp_clone/features/auth/controller/auth_controller.dart';
 import 'package:whatsapp_clone/features/select_contacts/screens/select_contact_screen.dart';
 import 'package:whatsapp_clone/features/chat/widgets/contacts_list.dart';
+import 'package:whatsapp_clone/features/status/screens/confirm_status_screen.dart';
+import 'package:whatsapp_clone/features/status/screens/status_contacts_screen.dart';
 
 class MobileScreenLayout extends ConsumerStatefulWidget {
   const MobileScreenLayout({super.key});
@@ -12,21 +17,26 @@ class MobileScreenLayout extends ConsumerStatefulWidget {
   ConsumerState<MobileScreenLayout> createState() => _MobileScreenLayoutState();
 }
 
-class _MobileScreenLayoutState extends ConsumerState<MobileScreenLayout> with WidgetsBindingObserver {
+class _MobileScreenLayoutState extends ConsumerState<MobileScreenLayout>
+    with WidgetsBindingObserver, TickerProviderStateMixin {
+  late TabController tabBarController;
   @override
   void initState() {
     super.initState();
+    tabBarController = TabController(length: 3, vsync: this);
     WidgetsBinding.instance.addObserver(this);
   }
+
   @override
   void dispose() {
     super.dispose();
     WidgetsBinding.instance.removeObserver(this);
   }
+
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     super.didChangeAppLifecycleState(state);
-    switch(state){
+    switch (state) {
       case AppLifecycleState.resumed:
         ref.read(authControllerProvider).setUserState(true);
         break;
@@ -35,9 +45,11 @@ class _MobileScreenLayoutState extends ConsumerState<MobileScreenLayout> with Wi
       case AppLifecycleState.paused:
         ref.read(authControllerProvider).setUserState(false);
         break;
-      default:break;
+      default:
+        break;
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -56,27 +68,48 @@ class _MobileScreenLayoutState extends ConsumerState<MobileScreenLayout> with Wi
             IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
             IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
           ],
-          bottom: const TabBar(
-            labelPadding: EdgeInsets.all(10),
-            labelStyle: TextStyle(fontWeight: FontWeight.w500, fontSize: 17),
+          bottom: TabBar(
+            controller: tabBarController,
+            labelPadding: const EdgeInsets.all(10),
+            labelStyle:
+                const TextStyle(fontWeight: FontWeight.w500, fontSize: 17),
             indicatorWeight: 4,
             indicatorColor: tabColor,
             unselectedLabelColor: Colors.grey,
             labelColor: tabColor,
-            tabs: [
+            tabs: const [
               Text("Chats"),
               Text("Updates"),
               Text("Calls"),
             ],
           ),
         ),
-        body: const ContactsList(),
+        body: TabBarView(
+          controller: tabBarController,
+          children: const [
+            ContactsList(),
+            StatusContactsScreen(),
+            Text("calls")
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, SelectContactScreen.routeName);
+          onPressed: () async {
+            if (tabBarController.index == 0) {
+              Navigator.pushNamed(context, SelectContactScreen.routeName);
+            } else {
+              File? pickedImage = await pickImageFromGallery(context);
+              if (pickedImage != null) {
+                Navigator.pushNamed(
+                  context,
+                  ConfirmStatusScreen.routeName,
+                  arguments: pickedImage,
+                );
+              }
+            }
           },
           backgroundColor: tabColor,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
           child: const Icon(Icons.comment),
         ),
       ),
